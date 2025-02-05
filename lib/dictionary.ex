c3443019db8b18789a6ddd0989e4c3c8d5ac6533
Alysson Silva <shirubaarison@gmail.com>
2025-02-05 11:15:35 -0300

    list_files

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -3,32 +3,57 @@ defmodule KindleExtractor.Dictionary do
   alias KindleExtractor.Tree
 
   def load_dictionary(dictionary) do
-    1..28
-    |> Enum.reduce([], fn n, acc ->
-      bank = "term_bank_#{n}.json"
-      case load_term_bank(dictionary, bank) do
-        [] -> acc
-        json -> [json | acc]
-      end
-    end)
-    |> Enum.concat()
+    folder_path = "dictionaries/" <> dictionary
+
+    case list_files(folder_path) do
+      {:ok, files} ->
+        file_count = Enum.reduce(files, 0, fn file, acc ->
+          case File.stat(Path.join(folder_path, file)) do
+            {:ok, _info} -> acc + 1
+            _ -> acc
+          end
+        end)
+
+        full_dict =
+          1..(file_count - 1) # do not load index.json
+            |> Enum.reduce([], fn n, acc ->
+              bank = "term_bank_#{n}.json"
+              case load_term_bank(folder_path, bank) do
+                [] -> acc
+                json -> [json | acc]
+              end
+            end)
+            |> Enum.concat()
+
+        {:ok, full_dict}
+      {:error} -> {:error, "Invalid dict name or not found"}
+    end
   end
 
-  defp load_term_bank(dictionary, bank) do
-    with {:ok, body} <- File.read("dictionaries/" <> dictionary <> "/" <> bank),
+  defp load_term_bank(path, bank) do
+    with {:ok, body} <- File.read(path <> "/" <> bank),
          {:ok, json} <- Jason.decode(body), do: json
   end
 
   def load_tree(json) do
-    Enum.reduce(json, nil, fn elem, tree ->
-      [word, reading, _, _, _, definition | _] = elem
-      Tree.insert(tree, word, reading, definition)
-    end)
+    tree =
+      Enum.reduce(json, nil, fn elem, tree ->
+        [word, reading, _, _, _, definition | _] = elem
+        Tree.insert(tree, word, reading, definition)
+      end)
+    {:ok, tree}
   end
 
   def search(tree, word) do
     case Tree.search(tree, word) do
       word -> word
+    end
+  end
+
+  defp list_files(path) do
+    case File.ls(path) do
+      {:ok, files} -> {:ok, files}
+      {:error, _reason} -> {:error}
     end
   end
 end
